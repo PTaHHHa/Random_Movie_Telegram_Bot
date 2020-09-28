7dd4b19d7202791e418866124d39d1885c10a6fe
PTaHHHa <sorokavyacheslav17735@gmail.com>
2020-09-28 20:17:14 +0300

    feat: all urls add

--- main.py
+++ main.py
@@ -1,49 +1,43 @@
-TODO: """НУЖНО СДЕЛАТЬ СЛОВАРЬ, В КОТОРОМ КЛЮЧАМИ БУДУТ ЖАНРЫ,
-А ЗНАЧЕНИЯМИ URL1(ДЛЯ ПОЛУЧЕНИЯ ПАГИНАЦИИ) И URL2(ДЛЯ ПРОБЕЖКИ ПО СТРАНИЦАМ)
-ВООБЩЕ МОЖНО ИСПОЛЬЗОВАТЬ ОДИН ЮРЛ, ПРОСТО БРАТЬ ЕГО ВМЕСТЕ С PAGE, ДАЖЕ ЕСЛИ СТРАНИЦА ОДНА
-А ПОТОМ УЖЕ МЕНЯТЬ ЗНАЧЕНИЕ PAGE, ЕСЛИ СТРАНИЦ БОЛЬШЕ ОДНОЙ.
-ТАК ВСЁ БУДЕТ РАБОТАТЬ НАМНОГО БЫСТРЕЕ.
-ЮРЛ НУЖНО БРАТЬ ТУТ https://www.kinopoisk.ru/popular/films/?sort=popularity&tab=all
-ТОЛЬКО НУЖНО ВЫБРАТЬ НЕОБХОДИМЫЕ ЖАНРЫ"""
+TODO: """РЕАЛИЗОВАТЬ САМОГО БОТА"""
 import requests
 from lxml import html
 import time
 
-start = ['документальный', 'боевик', 'фантастика', 'вестерн', 'драма', 'музыка', 'детектив', 'биография', 'аниме',
-         'ужасы',
-         'семейный', 'детский', 'фэнтези', 'приключения', 'военный', 'история', 'триллер', 'мелодрама', 'мультфильм',
-         'спорт', 'комедия', 'криминал', 'мюзикл']
-
 
 def main(genre):
-    if genre == 'аниме':
-        url = 'https://www.kinopoisk.ru/popular/films/anime/?sort=popularity&tab=all'
+    genre_dict = {'боевик': 'action/', 'фантастика': 'sci-fi/', 'драма': 'drama/', 'детектив': 'mystery/',
+                  'биография': 'biography/', 'аниме': 'anime/',
+                  'ужасы': 'horror/', 'фэнтези': 'fantasy/', 'приключения': 'adventure/', 'триллер': 'thriller/',
+                  'мелодрама': 'romance/', 'мультфильм': 'animation/',
+                  'комедия': 'comedy/', 'криминал': 'crime/'}
+    url = f'https://www.kinopoisk.ru/popular/films/{genre_dict.get(genre)}?sort=popularity&tab=all'
+    r = requests.get(url)
+    print(r.status_code)
+    t = html.fromstring(r.content.decode('utf-8'))
+    try:
+        page = t.xpath("//a[@class='paginator__page-number']/text()")[-1]
+    except IndexError:
+        page = 1
+    movie_list = []
+    for page_index in range(1, int(page) + 1):
+        print(f'Page: {page_index}')
+        url = f'https://www.kinopoisk.ru/popular/films/{genre_dict.get(genre)}?page={page_index}&sort=popularity&tab=all'
+        print(url)
         r = requests.get(url)
-        print(r.status_code)
         t = html.fromstring(r.content.decode('utf-8'))
-        try:
-            page = t.xpath("//a[@class='paginator__page-number']/text()")[-1]
-        except IndexError:
-            page = 1
-        movie_list = []
-        for page_index in range(int(page)):
-            print(f'Page: {page_index}')
-            url = f'https://www.kinopoisk.ru/popular/films/anime/?page={page_index}&sort=popularity&tab=all'
-            r = requests.get(url)
-            t = html.fromstring(r.content.decode('utf-8'))
-            for i in range(30):
-                genre_list = t.xpath(f"//div[@class='desktop-rating-selection-film-item']"
-                                     f"[{i}]/div[2]/div[1]/div[1]/div/a/p[3]/span[2]/text()")
-                for item in genre_list:
-                    # print(no_movie)
-                    if genre in item:
-                        name = t.xpath("//p[@class='selection-film-item-meta__name']/text()")[i - 1]
-                        movie_list.append(name)
-                    else:
-                        break
-        print(movie_list)
-        r.close()
+        no_movie = t.xpath("//span[@class='rating__value rating__value_positive']/text()")
+        for i in range(30):
+            genre_list = t.xpath(f"//div[@class='desktop-rating-selection-film-item']"
+                                 f"[{i}]/div[2]/div[1]/div[1]/div/a/p[3]/span[2]/text()")
+            for item in genre_list:
+                if genre in item and not '%' in no_movie:
+                    name = t.xpath("//p[@class='selection-film-item-meta__name']/text()")[i - 1]
+                    movie_list.append(name)
+                else:
+                    pass
+    print(movie_list)
+    r.close()
 
 
 if __name__ == '__main__':
-    main('аниме')
+    main('комедия')
